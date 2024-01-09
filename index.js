ead1a943805d04d26c10706f07fe6d2aa743c0be
aldrinreis <40066614+aldrinreis@users.noreply.github.com>
2024-01-09 00:43:58 +0000

    Exemplo

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ cadastrarUsuario()
   .catch((mensagem) => {
     console.log(mensagem);
   });
+
+function baixarConteudoPromisse(nomeConteudo) {
+  return new Promise((resolve, reject) => {
+    console.log(`Iniciando o download de ${nomeConteudo}.....`);
+
+    // Simula o tempo de download.
+
+    setTimeout(() => {
+      const sucesso = true; // Simula download bem-sucedido.
+      if (sucesso) {
+        console.log(`${nomeConteudo} foi baixado com sucesso!.....`);
+        resolve(nomeConteudo);
+      } else {
+        const erro = "Erro ao baixar o conteúdo.";
+        console.error(erro);
+        reject(erro); // Rejeita a promessa com o erro.
+      }
+    }, 2000);
+  });
+}
+
+// Usar a função baixarConteudoPromisse com Promise
+
+baixarConteudoPromisse("imagem.jpg")
+  .then((nomeConteudo) => {
+    console.log(`Download de ${nomeConteudo} concluído com sucesso!...`);
+    return baixarConteudoPromisse("Vídeo.mp4");
+  })
+  .then((nomeConteudo) => {
+    console.log(`Download de ${nomeConteudo} concluído com sucesso!...`);
+  })
+  .catch((erro) => {
+    console.error(`Erro ao baixar o conteúdo: ${erro}`);
+  });
